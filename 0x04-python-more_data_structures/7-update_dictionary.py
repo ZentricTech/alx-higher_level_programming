167662dffcb16ca4a58e401d7f8258c265c2a528
root <zentrictech@gmail.com>
2024-01-11 01:04:48 -0800

    0x04-python-more_data_structure

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is not None:
-        dic2 = {key: value}
-        a_dictionary.update(dic2)
-        return(a_dictionary)
-    else:
-        return None
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return my_dict
 
-# creates if non-existent; updates even if exists
-# return a_dictionary[key] = value
