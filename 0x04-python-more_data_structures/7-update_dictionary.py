7ea6b8719968fa2713ac7928cc95a264afbc3477
root <zentrictech@gmail.com>
2024-01-11 01:28:29 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+
+
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is not None:
-        dic2 = {key: value}
-        a_dictionary.update(dic2)
-        return(a_dictionary)
-    else:
-        return None
+    n_dic = {key: value}
+    a_dictionary.update(n_dic)
+    return a_dictionary
 
