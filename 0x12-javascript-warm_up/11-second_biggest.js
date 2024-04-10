94e94b06b9ecbc8e37e8d720ebfed0878102a0b6
root <zentrictech@gmail.com>
2024-04-10 17:52:43 +0000

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
-  console.log('0');
-} else {
-  const arr = process.argv.slice(2).map(Number);
-  const second = arr.sort(function (a, b) { return b - a; })[1];
-  console.log(second);
+
+const _array = process.argv.slice(2);
+
+function secondToMax (_array) {
+  if (_array.length < 2) {
+    return (0);
+  } else {
+    _array.sort((x, y) => x - y);
+    _array.pop();
+    return (_array.pop());
+  }
+}
+console.log(secondToMax(_array));
