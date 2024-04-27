d5a66f69065aece4e7304c08681ecb3e8519d0dd
root <zentrictech@gmail.com>
2024-04-27 08:28:44 -0700

    0x10-python-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to that URL, and displays the size of the body of the response
-curl -s "${1}" | wc -c
+# Sends request to URL, & display size of the body of the response
+curl -sI "$1" | grep "Content-Length:" | cut -f2 -d' '
