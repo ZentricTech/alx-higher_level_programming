c55415c16f14f3b0bc304e4beae305f11d33c592
root <zentrictech@gmail.com>
2024-06-18 10:46:58 -0700

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 const dKeys = Object.keys(dict);
-const convertedArr = Object.entries(dict);
 const values = Object.values(dict);
 let matched;
-const newObj = {};
 const result = {};
 // loop over the values
 for (let i = 0; i < values.length; i++) {
@@ -13,8 +11,3 @@ for (let i = 0; i < values.length; i++) {
   matched.forEach(item => result[JSON.stringify(values[i])].push(item));
 }
 console.log(result)
-convertedArr.forEach(element => {
-  newObj[element[1]] ? newObj[element[1]].push(element[0]) : newObj[element[1]] = [element[0]];
-});
-
-console.log(newObj);
