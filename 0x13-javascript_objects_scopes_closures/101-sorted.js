df3a8d1aaa038e158bacd1122bee318aa5d5dff8
root <zentrictech@gmail.com>
2024-06-18 11:04:10 -0700

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
+const { dict } = require('./101-data');
 const dKeys = Object.keys(dict);
 const values = Object.values(dict);
 let matched;
