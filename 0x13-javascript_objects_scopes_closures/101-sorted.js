75393291f08b266811cf1c91b4bf6f510ebebde5
root <zentrictech@gmail.com>
2024-06-18 10:24:30 -0700

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-
 const dict = require('./101-data').dict;
-
-let newDic = {};
-for (let key in dict) {
-  if (newDic[dict[key]] === undefined) {
-    newDic[dict[key]] = [];
-  }
-  newDic[dict[key]].push(key);
+const dKeys = Object.keys(dict);
+const values = Object.values(dict);
+let matched;
+const result = {};
+// loop over the values
+for (let i = 0; i < values.length; i++) {
+  result[JSON.stringify(values[i])] = [];
+  matched = dKeys.filter(key => dict[key] === values[i]);
+  matched.forEach(item => result[JSON.stringify(values[i])].push(item));
 }
-
-console.log(newDic);
+console.log(result)
