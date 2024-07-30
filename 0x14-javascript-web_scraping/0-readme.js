9cf1f40c2fb807f4b23e850f970a225b369659c1
root <zentrictech@gmail.com>
2024-07-30 03:21:43 -0700

    0x14-javascript-web_scrapin

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,18 @@
 #!/usr/bin/node
-// Read from file
 
-const filesys = require('fs');
-filesys.readFile(process.argv[2], 'utf-8',
-  function (err, data) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log(data);
-  });
+// Import the 'fs' module for file system operations
+const fs = require('fs');
+
+// Get the filename from the command-line arguments
+const filename = process.argv[2];
+
+// Use 'fs.readFile' to read the file asynchronously
+fs.readFile(filename, 'utf-8', (err, content) => {
+  // If an error occurred, log it to the console
+  if (err) {
+    console.log(err);
+  } else {
+    // Otherwise, print the content of the file
+    console.log(content);
+  }
+});
